ebb75f98cf606b9b4882728f1e587916c24e3c06
MateuszG-ux <mateusz.galezia@gmail.com>
2025-07-19 19:29:05 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const scrollBtn = document.getElementById('scrollToTop');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) { // po przewinięciu 300px pokazujemy przycisk
+    scrollBtn.classList.add('show');
+  } else {
+    scrollBtn.classList.remove('show');
+  }
+});
+
+scrollBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
